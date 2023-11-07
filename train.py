89b47290ff125c02a4ad634539690ecbf3d1d10b
Shichang Liu <klawens@memtrail.com>
2023-11-07 17:50:20 +0800

    remove main.py

--- train.py
+++ train.py
@@ -271,3 +271,50 @@ def eval(modelConfig: Dict):
         save_image(sampledImgs, os.path.join(
             modelConfig["sampled_dir"],  modelConfig["sampledImgName"]), nrow=modelConfig["nrow"])
 
+
+def main(model_config = None):
+    modelConfig = {
+        "state": "train", # train or eval
+        "epoch": 501,
+        "batch_size": 6,
+        "T": 50,
+        "channel": 128,
+        "channel_mult": [1, 2, 3, 4],
+        "attn": [2],
+        "num_res_blocks": 2,
+        "dropout": 0.15,
+        "lr": 1e-4,
+        "multiplier": 2.,
+        "beta_1": 1e-4,
+        "beta_T": 0.02,
+        "img_size": 480,
+        "patch_size": 32,
+        "grad_clip": 1.,
+        "device": "cuda:0",
+        "data_dir": "/home/klawens/LOL/",
+        "pretrained": False,
+        "pretrained_weight_dir": "./pretrained/",
+        "decom_pre": "decom_pretrained.pt",
+        "diff_pre": "diff_pretrained.pt",
+        "resume": False,
+        "training_load_weight": "diff_latest.pt",
+        # "training_load_weight": None,
+        "decomp_weight": "decom_latest.pt",
+        # "decomp_weight": None,
+        "save_weight_dir": "/home/klawens/work_0/checkpoints/",
+        "test_load_weight": "ckpt_1000_.pt",
+        "sampled_dir": "./SampledImgs/",
+        "sampledNoisyImgName": "NoisyNoGuidenceImgs.png",
+        "sampledImgName": "rec_illum.png",
+        "nrow": 2
+        }
+    if model_config is not None:
+        modelConfig = model_config
+    if modelConfig["state"] == "train":
+        train(modelConfig)
+    else:
+        eval(modelConfig)
+
+
+if __name__ == '__main__':
+    main()
